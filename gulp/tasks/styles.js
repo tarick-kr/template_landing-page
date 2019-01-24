d8dd88552fefaeda5943a959433c70656c45e37e
Sergey <tsg-kr@ukr.net>
2019-01-24 22:11:15 +0200

    Initial commit

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,48 @@
+let plumber = require('gulp-plumber'),
+	sass = require('gulp-sass'),
+	autoprefixer = require('gulp-autoprefixer'),
+	csso = require('gulp-csso'),
+	csscomb = require('gulp-csscomb'),
+	sourcemaps = require('gulp-sourcemaps'),
+	rename = require('gulp-rename'),
+	stylesPATH = {
+		"input": "./dev/static/styles/",
+		"ouput": "./build/static/css/"
+	};
+
+module.exports = function () {
+	$.gulp.task('styles:dev', () => {
+		return $.gulp.src(stylesPATH.input + 'styles.sass')
+			.pipe(plumber())
+			.pipe(sourcemaps.init())
+			.pipe(sass())
+			.pipe(autoprefixer({
+				browsers: ['last 3 version']
+			}))
+			.pipe(sourcemaps.write())
+			.pipe(rename('styles.css'))
+			.pipe($.gulp.dest(stylesPATH.ouput))
+			.on('end', $.browserSync.reload);
+	});
+	$.gulp.task('styles:build', () => {
+		return $.gulp.src(stylesPATH.input + 'styles.sass')
+			.pipe(sass())
+			.pipe(autoprefixer({
+				browsers: ['last 3 version']
+			}))
+			.pipe(csscomb())
+			.pipe(rename('styles.css'))
+			.pipe($.gulp.dest(stylesPATH.ouput))
+	});
+	$.gulp.task('styles:build-min', () => {
+		return $.gulp.src(stylesPATH.input + 'styles.sass')
+			.pipe(sass())
+			.pipe(autoprefixer({
+				browsers: ['last 3 version']
+			}))
+			.pipe(csscomb())
+			.pipe(csso())
+			.pipe(rename('styles.min.css'))
+			.pipe($.gulp.dest(stylesPATH.ouput))
+	});
+};
